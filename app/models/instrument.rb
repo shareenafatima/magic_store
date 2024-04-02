66b3724d05dac54f8f1602ec17c6192ce6b258d9
shareena fatima <shareenafatima40@gmail.com>
2024-04-02 20:55:59 +0500

    adding  style

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -4,7 +4,7 @@ class Instrument < ApplicationRecord
 
 
   validates :title, :brand, :price, :model, presence: true
-  validates :description, length: {maximum:1000, too_long: "%{count} character is the maximum allowed."}
+  validates :description, length: {maximum:10000, too_long: "%{count} character is the maximum allowed."}
   validates :title, length: {maximum:140, too_long: "%{count} character is the maximum allowed."}
   validates :price, numericality: {only_integer: true}, length: {maximum:7}
 
